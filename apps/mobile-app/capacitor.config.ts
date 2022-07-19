07a687bd0406179cde14800e49f708be9e125457
HowTommy <howtommy.net@gmail.com>
2022-07-19 23:04:11 +0200

    generated the project with blank api and app

---
+++ apps/mobile-app/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'mobile-app',
+  webDir: '../../dist/apps/mobile-app',
+  bundledWebRuntime: false,
+};
+
+export default config;
